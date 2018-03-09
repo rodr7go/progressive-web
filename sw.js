c4341ca5ffe5fa594a216504023ff7c1aa22b493
Eduardo Ortega <ortegon000@hotmail.com>
2018-03-09 15:24:22 -0600

    x

--- sw.js
+++ sw.js
@@ -1,28 +1,28 @@
-const CACHE_NAME = 'pwa-demo-edteam-cache-v1'
-let baseUrl = 'https://rodr7go.github.io/progressive-web/';
-urlsToCache = [
-    'https://controlmas.mx',
-    '/',
+const CACHE_NAME = 'pwa-demo-edteam-cache-v1',
+  urlsToCache = [
     './',
     './?utm=homescreen',
     './index.html',
-    './index.html/?utm=homescreen',
+    './index.html?utm=homescreen',
     './style.css',
     './script.js',
-    './sw.js',
     './favicon.ico',
+    './img/icon_192x192.png',
     'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css'
-]
-self.addEventListener('install', e =>  {
-    console.log('Evento: SW Instalado')
-    e.waitUntil(
-        caches.open(CACHE_NAME)
-            .then(cache => {
-                console.log('Archivos en cache')
-                return cache.addAll(urlsToCache)
-            })
-            .catch(err => console.log('Fallo registro de cache', err))
-    )
+  ]
+
+self.addEventListener('install', e => {
+  console.log('Evento: SW Instalado')
+  e.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Archivos en cache')
+        return cache.addAll(urlsToCache)
+        .then( () => self.skipWaiting() )
+        //skipWaiting forza al SW a activarse
+      })
+      .catch(err => console.log('Falló registro de cache', err) )
+  )
 })
 
 self.addEventListener('activate', e => {
@@ -48,27 +48,19 @@ self.addEventListener('activate', e => {
 })
 
 self.addEventListener('fetch', e => {
-    console.log('Evento: SW Recuperando')
-    e.respondWith(
-        caches.match(e.request)
-            .then(res => {
-                if ( res ) {
-                    return res
-                }
-                let request
-                return fetch(request)
-                    .then(res => {
-                        let resToCache = res.clone()
-                        let cacheName
-                        caches.open(cacheName)
-                            .then(cache => {
-                                cache
-                                    .put(request, resToCache)
-                                    .catch(err => console.log(`${request.url}, ${err.message}`))
-                            })
+  console.log('Evento: SW Recuperando')
 
-                        return res
-                    })
-            })
+  e.respondWith(
+    //Miramos si la petición coincide con algún elemento del cache
+    caches.match(e.request)
+      .then(res => {
+        console.log('Recuperando cache')
+        if ( res ) {
+          //Si coincide lo retornamos del cache
+          return res
+        }
+        //Sino, lo solicitamos a la red
+        return fetch(e.request)
+      })
     )
 })
